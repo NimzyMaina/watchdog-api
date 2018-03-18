355e20c7a3dcd995e2f9e82f4d3c82c19f6e0293
Nimrod Maina <nimzy.maina@gmail.com>
2018-03-18 18:55:18 +0300

    - retry

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 $s = 'social.';
-Route::get('/social/redirect/{provider}',   ['as' => $s . 'redirect','uses' => 'Auth\SocialController@getSocialRedirect']);
-Route::get('/social/handle/{provider}',     ['as' => $s . 'handle', 'uses' => 'Auth\SocialController@getSocialHandle']);
+Route::get('/social/redirect/{provider}',['as' => $s . 'redirect','uses' => 'Auth\SocialController@getSocialRedirect']);
+Route::get('/social/handle/{provider}',['as' => $s . 'handle', 'uses' => 'Auth\SocialController@getSocialHandle']);
 
 
 Route::get('/home', 'HomeController@index')->name('home');

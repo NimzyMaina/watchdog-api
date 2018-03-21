5e1f9af54be02535fbfffa4abb4233e11794f21e
Nimrod Maina <nimzy.maina@gmail.com>
2018-03-21 16:16:17 +0300

    - login patch

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -77,7 +77,7 @@ class SocialController extends Controller
                 }
 
                 $newSocialUser->password = bcrypt(str_random(16));
-                $newSocialUser->token = str_random(64);
+                $newSocialUser->api_token = str_random(64);
                 $newSocialUser->phone = '';
                 $newSocialUser->is_active = true;
                 $newSocialUser->save();
